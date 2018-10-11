e2d142c0df3854a4552719cce0e693c069f7556f
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2018-10-11 18:14:55 +0100

    Setting Up - Login View
    Login / Password

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller {
+public class Controller implements ActionListener {
+	
+	View view;
+	Model model;
+	
+	public Controller() {
+		this.view = new View();
+		this.model = new Model();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/View.java
+++ src/View.java
@@ -1,4 +1,75 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
-public class View {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class View extends JFrame{
+
+	public View() {
+		
+		this.setVisible(true);
+		this.setSize(800, 600);
+		this.setTitle("Online Barber's Shop Appointments");
+		setupLoginFrame();
+		
+	}
+	
+	public void setupLoginFrame() {
+		
+		BorderLayout boder = new BorderLayout();
+		this.setLayout(boder);
+		
+		//GridLayout grid = new GridLayout();
+		//this.setLayout(grid);
+		
+		JPanel central = new JPanel();
+		JPanel top = new JPanel();
+		JPanel bottom = new JPanel();
+		
+		
+		//////////////////////////////////TOP///////////////////////////////////////
+		
+		JLabel welcome = new JLabel("Welcome to the System Appointments");
+		top.add(welcome);
+		
+		////////////////////////////CENTER//////////////////////////////////////////
+		
+		JLabel enterLogin = new JLabel("Login");
+		central.add(enterLogin);
+				
+		JTextField user = new JTextField(20);
+		central.add(user);
+		
+		JLabel enterPassword = new JLabel("Password");
+		central.add(enterPassword);
+		
+		JTextField password = new JTextField(20);
+		central.add(password);
+		
+		////////////////////////////////BOTTOM//////////////////////////////////////////
+		
+		JLabel register = new JLabel("New Customer?");
+		bottom.add(register);
+		
+		JButton regButton = new JButton("Register here");
+		bottom.add(regButton);
+		
+		///////////////////////////////////////////////////////////////////////////////
+		
+		
+		this.add(top, BorderLayout.PAGE_START);
+		this.add(central, BorderLayout.CENTER);
+		this.add(bottom, BorderLayout.PAGE_END);
+		
+		
+		this.validate();
+		this.repaint();
+	}
+	
+	
+	
 }

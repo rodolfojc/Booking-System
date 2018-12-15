068da23a784285efa827d02aaddf4ec7cd601d47
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2018-12-15 21:52:44 +0000

    CustomerController - Cleaning Up
    Comments added.

--- src/CustomerController.java
+++ src/CustomerController.java
@@ -4,12 +4,15 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 public class CustomerController implements ActionListener{
-
+	
+	// GLOBAL VARIABLES - DECLARATION
 	CustomerView custView;
 	Login login;
 	
+	// CONSTRUCTOR
 	public CustomerController(String email, Login Login){
 		
+		// NEW INSTANCE OF CUSTOMERVIEW THAT ALSO INSTANCE AN VIEW OBJECT FOR CUSTOMER VIEW
 		this.custView = new CustomerView(this, email);
 		this.login = Login;
 		
@@ -18,24 +21,25 @@ public class CustomerController implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+		//TO SEARCH AVAILABILITY BY NAME OR LOCATION
 		if (e.getActionCommand().equals("Search")){
 			
 			CustomerDBQ custDB = new CustomerDBQ(this.custView);
-			//Database data = new Database(this.custView);
 			custDB.searchProvider(custView.getByOption(), custView.getByField());
 			this.custView.UpdateFrame(false);
 		}
 		
+		//FOR GET APPOINTMENT SELECTED ON TABLE BY AVAILABILITY REFERENCE
 		if (e.getActionCommand().equals("Get Appoint")) {
 			
 			CustomerDBQ custDB = new CustomerDBQ(this.custView);
-			//Database data = new Database(this.custView);
 			String avai_ref = custDB.bookAppointment();
 			CustomerDBQ custDBTwo = new CustomerDBQ(this.custView);
-			//Database dataTwo = new Database(this.custView);
 			custDBTwo.setAppointment(avai_ref);
 		}
 		
+		// TO CANCEL APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if (e.getActionCommand().equals("Cancel")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "Appointment has been CANCELLED!";
@@ -44,6 +48,7 @@ public class CustomerController implements ActionListener{
 			this.custView.UpdateFrame(false);
 		}
 		
+		//TO SET A COMMENT TO AN APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if(e.getActionCommand().equals("Set")) {
 			String oldcomment = "Comment: "+this.custView.getTableStatus(this.custView.getSelectedRowTTwo(), 6);
 			String comment = (String)JOptionPane.showInputDialog(this.custView,
@@ -62,12 +67,13 @@ public class CustomerController implements ActionListener{
 			
 		}
 		
+		// TO LOGOUT
 		if(e.getActionCommand().equals("Logout")) {
 			JOptionPane.showMessageDialog(this.custView, "See you soon!", "Logout", JOptionPane.INFORMATION_MESSAGE);
 			this.custView.getCustView().dispose();
 			this.login.getLogin().setVisible(true);
 		}
-		
+		// TO SHOW COMMENTS OF N APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if(e.getActionCommand().equals("Show")) {
 			
 			JOptionPane.showMessageDialog(this.custView, this.custView.getTableStatus(this.custView.getSelectedRowTTwo(), 6));

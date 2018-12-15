f78e01cea5bbd62d78b59eea9c8480a6a4014707
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2018-12-15 20:25:22 +0000

    Database Class - Cleaning Up
    Comments added,.

--- src/Database.java
+++ src/Database.java
@@ -8,132 +8,121 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class Database {
-	
+
+	// GLOBAL VARIABLES - DECLARATION
 	Connection conn = null;
 	Statement stmt = null;
 	ResultSet rs = null;
 	Register register;
 	Login login;
-	//CustomerView custView;
-	//ProviderView proView;
-	//AdminView adminView;
-	
+
+	// MAIN CONSTRUCTOR
 	public Database() {
 		connectDB();
 	}
-	
+
+	// CONTSRUCTOR FOR REGISTER VIEW AS A PARAMETER
 	public Database(Register register) {
-		
+
 		this.register = register;
 		connectDB();
 	}
-	
+
+	// CONTSRUCTOR FOR LOGIN VIEW AS A PARAMETER
 	public Database(Login login) {
-		
+
 		this.login = login;
 		connectDB();
 	}
-	
-	/*public Database(ProviderView proView) {
-		this.proView = proView;
-		connectDB();
-	}
-	
-	public Database(Login login) {
-		
-		this.login = login;
-		connectDB();
-	}
-	
-	public Database(CustomerView custView) {
-		this.custView = custView;
-		connectDB();
-	}
-	
-	public Database(CustomerView custView, ProviderView proView) {
-		this.custView = custView;
-		this.proView = proView;
-		connectDB();
-	}*/
-	
+
+	// DATABSE CONNECTION
 	public void connectDB() {
-		
-		try{
+
+		try {
 			// Load the database driver
-			Class.forName("com.mysql.jdbc.Driver").newInstance() ;
-			
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
 			String dbServer = "jdbc:mysql://localhost:3306/booking";
 			String user = "root";
-			String password ="";
+			String password = "";
 
 			// Get a connection to the database
-			conn = DriverManager.getConnection(dbServer, user, password) ;
+			conn = DriverManager.getConnection(dbServer, user, password);
 
 			// Get a statement from the connection
-			stmt = conn.createStatement() ;
+			stmt = conn.createStatement();
 
-
-		}catch( SQLException se ){
-			System.out.println( "SQL Exception:" ) ;
+		} catch (SQLException se) {
+			System.out.println("SQL Exception:");
 
 			// Loop through the SQL Exceptions
-			while( se != null ){
-				System.out.println( "State  : " + se.getSQLState()  ) ;
-				System.out.println( "Message: " + se.getMessage()   ) ;
-				System.out.println( "Error  : " + se.getErrorCode() ) ;
+			while (se != null) {
+				System.out.println("State  : " + se.getSQLState());
+				System.out.println("Message: " + se.getMessage());
+				System.out.println("Error  : " + se.getErrorCode());
 
-				se = se.getNextException() ;
+				se = se.getNextException();
 			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		catch( Exception e ){
-			System.out.println( e ) ;
-		}
-	
-		
+
 	}
-	
+
+	// METHOD TO REGISTER AN USER, TYPE NEEDED
 	public void registerUser(String type) {
-		
-		boolean flag=true;
-		
+
+		// FLAG FOR JOPTIONPANE MESSAGES
+		boolean flag = true;
+
+		// FOR CUSTOMER
 		if (type.equals("Customer")) {
 			try {
-				
+
+				// STATEMENT
+				// SHA2 IS THE TYPE OF ENCRYPTION IN MYSQL TO SAVE THE PASSWORD
 				String query = "INSERT INTO customers (cust_name, cust_surname, mob_num, email, address, pass)"
 						+ "VALUES (?, ?, ?, ?, ?, SHA2(?,512))";
-			
+
+				// PREPARATION
 				PreparedStatement preparedStmt = conn.prepareStatement(query);
 				preparedStmt.setString(1, this.register.getName());
 				preparedStmt.setString(2, this.register.getSur());
-				preparedStmt.setString(3, this.register.getMob());;
+				preparedStmt.setString(3, this.register.getMob());
+				;
 				preparedStmt.setString(4, this.register.getEmail());
 				preparedStmt.setString(5, this.register.getAddress());
 				preparedStmt.setString(6, this.register.getPassField());
-								
+
 				preparedStmt.execute();
 				conn.close();
-				
-			}catch (Exception e)
-		    	{
-			      JOptionPane.showMessageDialog(this.register, "Ups, there is a problem, try again!");
-			      flag=false;
-				  System.err.println("Got an exception!");
-			      System.err.println(e.getMessage());
-			    }
-			
-			if(flag==true) JOptionPane.showMessageDialog(this.register, "Your account has been created "
-					+this.register.getName()+" . Now you can login, thanks");
+
+			} catch (Exception e) {
+				// ERROR MESSAGES
+				JOptionPane.showMessageDialog(this.register, "Ups, there is a problem, try again!");
+				flag = false;
+				System.err.println("Got an exception!");
+				System.err.println(e.getMessage());
+			}
+
+			// NO ERRORS
+			if (flag == true)
+				JOptionPane.showMessageDialog(this.register,
+						"Your account has been created " + this.register.getName() + " . Now you can login, thanks");
 			this.register.getReg().dispose();
-			
+
 		}
-		
+
+		// FOR PROVIDERS
+		// BASICALLY THE SAME CODE AS CUSTOMER BUT WITH LOCATION, AND REGISTRATION DATA
+		// ADDED.
 		if (type.equals("Provider")) {
-			
-				try {
-				
+
+			try {
+
 				String query = "INSERT INTO providers (pro_name, pro_surname, mob_num, email, address, pass, location, status, reg_day)"
 						+ "VALUES (?, ?, ?, ?, ?, SHA2(?,512), ?, ?, ?)";
-			
+
 				PreparedStatement preparedStmt = conn.prepareStatement(query);
 				preparedStmt.setString(1, this.register.getName());
 				preparedStmt.setString(2, this.register.getSur());
@@ -144,436 +133,115 @@ public class Database {
 				preparedStmt.setString(7, this.register.getLoc());
 				preparedStmt.setString(8, "Pending");
 				preparedStmt.setDate(9, this.register.getRegDatE());
-				
+
 				preparedStmt.execute();
 				conn.close();
-				
-			}catch (Exception e)
-		    	{
-				  JOptionPane.showMessageDialog(this.register, "Ups, there is a problem, try again!");
-			      flag=false;
-			      System.err.println("Got an exception!");
-			      System.err.println(e.getMessage());
-			    }
-		
-			if(flag==true) JOptionPane.showMessageDialog(this.register, "Your account has been created "
-						+this.register.getName()+" . Please, wait until an administrator validate your account, thanks");
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(this.register, "Ups, there is a problem, try again!");
+				flag = false;
+				System.err.println("Got an exception!");
+				System.err.println(e.getMessage());
+			}
+
+			if (flag == true)
+				JOptionPane.showMessageDialog(this.register, "Your account has been created " + this.register.getName()
+						+ " . Please, wait until an administrator validate your account, thanks");
 			this.register.getReg().dispose();
 		}
-		
+
 	}
-	
+
+	// METHOD TO CHECK IF THE CUSTOMER OR PROVIDER IS REGISTERED, RETURN TRUE IS
+	// THERE IS A MATCH
 	public boolean loginUser(String user, String email, String password, boolean flag) {
-		
+
 		String query;
-		
+
+		// QUERIES FOR DIFFERENT TYPE
 		if (flag) {
-			query = "SELECT * FROM "+user+" WHERE admin_user = '" + email + "' AND admin_pass= SHA2('"+ password + "',512);";
-		}else {
-			query = "SELECT * FROM "+user+" WHERE email = '" + email + "' AND pass= SHA2('"+ password + "',512);";
+			query = "SELECT * FROM " + user + " WHERE admin_user = '" + email + "' AND admin_pass= SHA2('" + password
+					+ "',512);";
+		} else {
+			query = "SELECT * FROM " + user + " WHERE email = '" + email + "' AND pass= SHA2('" + password + "',512);";
 		}
-		
+
 		try {
-			rs = stmt.executeQuery(query) ;
-			
+			rs = stmt.executeQuery(query);
+
 			// This code is telling us whether we have any results
 			// in our database or not
-			if (rs.isBeforeFirst()){
+			if (rs.isBeforeFirst()) {
 				return true;
 			}
-			
+
 			// Close the result set, statement and the connection
-			rs.close() ;
-			stmt.close() ;
-			conn.close() ;
-			
+			rs.close();
+			stmt.close();
+			conn.close();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
+	// METHOD TO GET THE PROVIDER STATUS AND RETURN THE STATUS.
 	public String getProStatus(String email) {
-		
-		String query = "SELECT status FROM providers WHERE email='"+email+"';";
-		String result="";
-		
+
+		String query = "SELECT status FROM providers WHERE email='" + email + "';";
+		String result = "";
+
 		try {
-			
+
 			rs = stmt.executeQuery(query);
-			
-			while(rs.next()) {
-				result=rs.getString("status");
+
+			while (rs.next()) {
+				result = rs.getString("status");
 			}
-			
+
 			rs.close();
-			stmt.close() ;
-			conn.close() ;
-			
-			
-		}catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		
+			stmt.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
 		}
 		return result;
 	}
-	
+
+	// METHOD TO CHECK IF THE EMAIL ADDRESS IS ALREADY REGISTER IN A TABLE IN
+	// DATABASE
+	// RETURN TRUE IS THERE IS A MATCH
 	public boolean emailVerification(String user, String email) {
-		
-				
-			String query = "SELECT * FROM "+user+" WHERE email = '" + email + "';";
-		
-		
+
+		String query = "SELECT * FROM " + user + " WHERE email = '" + email + "';";
+
 		try {
-			rs = stmt.executeQuery(query) ;
-			
+			rs = stmt.executeQuery(query);
+
 			// This code is telling us whether we have any results
 			// in our database or not
-			if (rs.isBeforeFirst()){
+			if (rs.isBeforeFirst()) {
 				return true;
 			}
-			
+
 			// Close the result set, statement and the connection
-			rs.close() ;
-			stmt.close() ;
-			conn.close() ;
-			
+			rs.close();
+			stmt.close();
+			conn.close();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
 }
 	
-	/*public void customerLogged() {
-		
-		
-		String query= "SELECT cust_id, cust_name, cust_surname FROM customers WHERE email='"+this.custView.getCustomerEmail()+"';";
-		
-		try {
-			
-			rs = stmt.executeQuery(query);
-		
-			while(rs.next()) {
-				custView.setCustomerID(rs.getInt("cust_id"));
-				custView.setCustomerName(rs.getString("cust_name"));
-				custView.setCustmerSurName(rs.getString("cust_surname"));
-			}
-			
-			rs.close();
-			stmt.close() ;
-			conn.close() ;
-				
-		}catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		}
-	}
-	
-	public void providerLogged() {
-		
-		
-		String query= "SELECT pro_id, pro_name, pro_surname, location FROM providers WHERE email='"+this.proView.getProviderEmail()+"';";
-		
-		try {
-			
-			rs = stmt.executeQuery(query);
-		
-			while(rs.next()) {
-				proView.setProviderID(rs.getInt("pro_id"));
-				proView.setProviderName(rs.getString("pro_name"));
-				proView.setProviderSurName(rs.getString("pro_surname"));
-				proView.setProviderLocation(rs.getString("location"));
-			}
-			
-			rs.close();
-			stmt.close() ;
-			conn.close() ;
-				
-		}catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	  }	
-	
-	public void addAvailability() {
-		
-		try {
-			
-			String query = "INSERT INTO availabilities (pro_id, date, time, available)"
-					+ "VALUES (?, ?, ?, ?)";
-		
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			preparedStmt.setInt(1, this.proView.getProviderID());
-			preparedStmt.setDate(2, this.proView.getDatE());
-			preparedStmt.setString(3, this.proView.getHour());
-			preparedStmt.setString(4, "Yes");
-			
-			preparedStmt.execute();
-			conn.close();
-			
-		}catch (Exception e)
-	    	{
-		      	JOptionPane.showMessageDialog(this.proView, "Ups, there is a error! try again later", "Data Error!", JOptionPane.ERROR_MESSAGE);
-				System.err.println("Got an exception!");
-				System.err.println(e.getMessage());
-		    }
-		JOptionPane.showMessageDialog(this.proView, "Your availability has been added");
-		this.proView.UpdateFrame();
-		
-	}
-	
-	public void toBeConfirmPro() {
-		
-		String query = "SELECT cust_name, cust_surname, date, time, availabilities.avai_ref FROM appointments "
-				+ "INNER JOIN customers ON customers.cust_id = appointments.cust_id "
-					+ "INNER JOIN availabilities ON availabilities.avai_ref = appointments.avai_ref "
-						+ "WHERE availabilities.pro_id = '"+this.proView.getProviderID()+"' "
-							+ "AND availabilities.available='Unconfirmed';";
-		String[][] data= new String[20][5];
-		
-		try {
-			
-			rs = stmt.executeQuery(query);
-			int i = 0;
-			
-			while(rs.next()) {
-				
-				data[i][0] = rs.getString("avai_ref");
-				data[i][1] = rs.getString("cust_name");
-				data[i][2] = rs.getString("cust_surname");
-				data[i][3] = rs.getString("date");
-				data[i][4] = rs.getString("time");
-				
-				i++;
-			}
-			
-			rs.close();
-			stmt.close() ;
-			conn.close() ;
-			
-			this.proView.setCopyDataBooked(data);
-			
-												
-		}catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	}
-		
-		public void availableProvTable() {
-			
-			String query = "SELECT date, time FROM availabilities av INNER JOIN providers pr ON av.pro_id = pr.pro_id  "
-					+ "WHERE email='"+this.proView.getProviderEmail()+"' AND available='Yes';";
-			String[][] data= new String[20][2];
-			
-			try {
-				
-				rs = stmt.executeQuery(query);
-				int i = 0;
-				
-				while(rs.next()) {
-					
-					data[i][0] = rs.getString("date");
-					data[i][1] = rs.getString("time");
-					i++;
-				}
-				
-				rs.close();
-				stmt.close() ;
-				conn.close() ;
-				
-				this.proView.setCopyDataAvai(data);
-				
-													
-			}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		    }
-		  
-		}
-		//ERASE
-		public void availableCosTable() {
-			
-			String query = "SELECT av.avai_ref, pr.pro_name, pr.pro_surname, av.date, av.time FROM availabilities av "
-					+ "INNER JOIN providers pr ON av.pro_id = pr.pro_id;";
-			String[][] data= new String[20][5];
-			
-			try {
-				
-				rs = stmt.executeQuery(query);
-				int i = 0;
-				
-				while(rs.next()) {
-					
-					data[i][0] = rs.getString("avai_ref");
-					data[i][1] = rs.getString("pro_name");
-					data[i][2] = rs.getString("pro_surname");
-					data[i][3] = rs.getString("date");
-					data[i][4] = rs.getString("time");
-					i++;
-				}
-				
-				rs.close();
-				stmt.close() ;
-				conn.close() ;
-				
-				this.custView.setCopyDataAvai(data);
-				
-					
-			}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		    }
-		  
-		
-						
-		}
-		
-		public String bookAppointment() {
-			
-						
-			try {
-				
-				String query = "INSERT INTO appointments (avai_ref, cust_id, comments) "
-						+ "VALUES (?, ?, ?)";
-			
-				PreparedStatement preparedStmt = conn.prepareStatement(query);
-				
-				preparedStmt.setString(1, this.custView.getDataAvai(this.custView.getSelectedRowT(),0));
-				preparedStmt.setInt(2, this.custView.getCustomerID());
-				preparedStmt.setString(3, "No");				
-				preparedStmt.execute();
-				conn.close();
-				
-			}catch (Exception e)
-		    	{
-			      	JOptionPane.showMessageDialog(this.custView, "Ups, there is a error! try again later", "Data Error!", JOptionPane.ERROR_MESSAGE);
-					System.err.println("Got an exception!");
-					System.err.println(e.getMessage());
-			    }
-			JOptionPane.showMessageDialog(this.custView, "Your appointment has been added");
-			
-			return this.custView.getDataAvai(this.custView.getSelectedRowT(),0);
-			
-		
-		}
-		
-		public void setAppointment(String avai_reference) {
-			
-			try {
-				
-				String query = "UPDATE availabilities SET available='Unconfirmed' WHERE avai_ref='"+avai_reference+"';";
-				
-				PreparedStatement preparedStmt = conn.prepareStatement(query);
-				preparedStmt.execute();
-				conn.close();
-				
-			}catch (Exception e)
-		    	{
-			      	
-					System.err.println("Got an exception!");
-					System.err.println(e.getMessage());
-			    }
-			this.custView.UpdateFrame(true);	
-		}
-		
-		public void comfirmAppointPro(String avai_reference) {
-			
-			try {
-				
-				String query = "UPDATE availabilities SET available='Confirmed' WHERE avai_ref='"+avai_reference+"';";
-				
-				PreparedStatement preparedStmt = conn.prepareStatement(query);
-				preparedStmt.execute();
-				conn.close();
-				
-			}catch (Exception e)
-		    	{
-			      	
-					System.err.println("Got an exception!");
-					System.err.println(e.getMessage());
-			    }
-			JOptionPane.showMessageDialog(this.proView, "The appointment has been confirmed. Thanks");
-			this.proView.UpdateFrame();	
-		}
-		
-			
-			
-			
-			
-		
-	
-		public void searchProvider(String by, String input) {
-			
-			String query="";
-			String[][] data= new String[20][5];
-			
-			try {
-			
-				if(by.equals("Name")) {
-					query = "SELECT avai_ref, pro_name, pro_surname, date, time FROM availabilities "
-						+ "INNER JOIN providers ON availabilities.pro_id = providers.pro_id WHERE providers.pro_name='"+input+"' AND "
-								+ "availabilities.available='Yes';";
-				}
-				else if(by.equals("Location")){
-					query = "SELECT avai_ref, pro_name, pro_surname, date, time FROM availabilities "
-							+ "INNER JOIN providers ON availabilities.pro_id = providers.pro_id WHERE providers.location='"+input+"' AND "
-								+ "availabilities.available='Yes';";
-				}
-				else {
-					query = "SELECT avai_ref, pro_name, pro_surname, date, time FROM availabilities "
-						+ "INNER JOIN providers ON availabilities.pro_id = providers.pro_id WHERE availabilities.available='Yes';";
-				}
-				
-				rs = stmt.executeQuery(query);
-				
-				int i = 0;
-				
-				while(rs.next()) {
-					
-					data[i][0] = rs.getString("avai_ref");
-					data[i][1] = rs.getString("pro_name");
-					data[i][2] = rs.getString("pro_surname");
-					data[i][3] = rs.getString("date");
-					data[i][4] = rs.getString("time");
-					i++;
-				}
-				
-				rs.close();
-				stmt.close() ;
-				conn.close() ;
-				
-				this.custView.setCopyDataAvai(data);
-				
-					
-			}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		    }
-			
-			
-		}*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
- 
-
-	
-

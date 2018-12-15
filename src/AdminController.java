504809fbadd9dd9c1c067dbb7be8cc15672ac23b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2018-12-15 22:07:01 +0000

    AdminController - Cleaning Up
    Comments added.

--- src/AdminController.java
+++ src/AdminController.java
@@ -5,136 +5,146 @@ import javax.swing.JOptionPane;
 
 public class AdminController implements ActionListener {
 
+	// GLOBAL VARIABLES - DECLARATION
 	private AdminView adminView;
 	private Login login;
-		
-	public AdminController (String AdminUser, Login Login) {
-		
+
+	// CONSTRUCTOR
+	public AdminController(String AdminUser, Login Login) {
+
+		// NEW INSTANCE OF ADMINVIEW THAT ALSO INSTANCE AN VIEW OBJECT FOR ADMIN VIEW
 		this.adminView = new AdminView(this, AdminUser);
 		this.login = Login;
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-		if(e.getActionCommand().equals("Delete Customer")) {
+
+		// TO DELECT A CUSTOMER SELECTED IN TABLE BY ID
+		if (e.getActionCommand().equals("Delete Customer")) {
 			String errorMg = "Ups, there is a problem, please be sure the customer does not have"
-	      			+ " any availability or appointment registered.";
+					+ " any availability or appointment registered.";
 			String confMg = "The user has been DELETED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.deleteRow("customers", "cust_ID", this.adminView.getDataCust(this.adminView.getSelectedRowCust(), 0), errorMg, confMg );
+			data.deleteRow("customers", "cust_ID", this.adminView.getDataCust(this.adminView.getSelectedRowCust(), 0),
+					errorMg, confMg);
 			adminView.UpdateFrame();
 		}
-		
-		if(e.getActionCommand().equals("Delete Provider")) {
+
+		// TO DELECT A PROVIDER SELECTED IN TABLE BY ID
+		if (e.getActionCommand().equals("Delete Provider")) {
 			String errorMg = "Ups, there is a problem, please be sure the provider does not have"
-	      			+ " any availability or appointment registered.";
+					+ " any availability or appointment registered.";
 			String confMg = "The user has been DELETED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.deleteRow("providers", "pro_ID", this.adminView.getDataPro(this.adminView.getSelectedRowPro(), 0), errorMg,confMg);
+			data.deleteRow("providers", "pro_ID", this.adminView.getDataPro(this.adminView.getSelectedRowPro(), 0),
+					errorMg, confMg);
 			adminView.UpdateFrame();
 		}
-		
+
+		// TO VALIDATE A CUSTOMER SELECTED IN TABLE BY ID (IN STATUS PENDING)
 		if (e.getActionCommand().equals("Validate Provider")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "Provider has been CONFIRMED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.updateRow("providers", "status", "Confirmed", "pro_id", this.adminView.getDataPro(this.adminView.getSelectedRowPro(), 0), errorMg, confMg);;
+			data.updateRow("providers", "status", "Confirmed", "pro_id",
+					this.adminView.getDataPro(this.adminView.getSelectedRowPro(), 0), errorMg, confMg);
+			;
 			adminView.UpdateFrame();
 		}
-		
-		if(e.getActionCommand().equals("Delete Availability")) {
+
+		// TO DELETE AVAILABILITY SELECTED ON TABLE BY AVAILABILITY REFERENCE
+		if (e.getActionCommand().equals("Delete Availability")) {
 			String errorMg = "Ups, there is a problem, please be sure the availability does not have"
-	      			+ " any appointment registered.";
+					+ " any appointment registered.";
 			String confMg = "The availability has been DELETED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.deleteRow("availabilities", "avai_ref", this.adminView.getDataAvai(this.adminView.getSelectedRowAvai(), 0), errorMg, confMg);
+			data.deleteRow("availabilities", "avai_ref",
+					this.adminView.getDataAvai(this.adminView.getSelectedRowAvai(), 0), errorMg, confMg);
 			adminView.UpdateFrame();
 		}
-		
-		if(e.getActionCommand().equals("Delete Appointment")) {
+
+		// TO DELETE APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
+		if (e.getActionCommand().equals("Delete Appointment")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "The appointment has been DELETED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.deleteRow("appointments", "appoint_ref", this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 0), errorMg, confMg);
-			
-			////////UPDATE CHILD ROW AVAI_REF TO CANCELLED////////
+			data.deleteRow("appointments", "appoint_ref",
+					this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 0), errorMg, confMg);
+
+			//////// UPDATE CHILD ROW AVAI_REF TO CANCELLED////////
 			String errorMgTwo = "Ups, there is an internal problem, please contact an administrator";
 			String confMgTwo = "Availability has been set CANCELLED!";
 			AdminDBQ dataTwo = new AdminDBQ(this.adminView);
-			dataTwo.updateRow("availabilities", "available", "Cancelled", "avai_ref", this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 1), errorMgTwo, confMgTwo);
+			dataTwo.updateRow("availabilities", "available", "Cancelled", "avai_ref",
+					this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 1), errorMgTwo, confMgTwo);
 			adminView.UpdateFrame();
 		}
-		
-		if(e.getActionCommand().equals("Manage")) {
-			String oldcomment = "Comment: "+this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 3);
-			String comment = (String)JOptionPane.showInputDialog(this.adminView,
-					oldcomment,
-					"Answer",
-					JOptionPane.PLAIN_MESSAGE,
-					null,
-					null,
-					null);
-			
+
+		// TO MANAGE/SET A COMMENT TO AN APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
+		if (e.getActionCommand().equals("Manage")) {
+			String oldcomment = "Comment: " + this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 3);
+			String comment = (String) JOptionPane.showInputDialog(this.adminView, oldcomment, "Answer",
+					JOptionPane.PLAIN_MESSAGE, null, null, null);
+
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "Your Comment have been SET!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.updateRow("appointments", "comments","Admin: "+comment+"", "appoint_ref", this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 0), errorMg, confMg);
+			data.updateRow("appointments", "comments", "Admin: " + comment + "", "appoint_ref",
+					this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 0), errorMg, confMg);
 			adminView.UpdateFrame();
-			
+
 		}
-		
-		if(e.getActionCommand().equals("Show")) {
-			
-			JOptionPane.showMessageDialog(this.adminView, this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 3));
-			
+
+		// TO SHOW COMMENTS OF AN APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
+		if (e.getActionCommand().equals("Show")) {
+
+			JOptionPane.showMessageDialog(this.adminView,
+					this.adminView.getDataAppoint(this.adminView.getSelectedRowAppoint(), 3));
+
 		}
-		
-		if(e.getActionCommand().equals("Add Admin")) {
-			String user = (String)JOptionPane.showInputDialog(this.adminView,
-					"Email address",
-					"Admin Register",
-					JOptionPane.PLAIN_MESSAGE,
-					null,
-					null,
-					null);
+
+		// TO CREATE AN ADMINSTRATOR, INPUTS MUST BE PROVIDED
+		if (e.getActionCommand().equals("Add Admin")) {
+			String user = (String) JOptionPane.showInputDialog(this.adminView, "Email address", "Admin Register",
+					JOptionPane.PLAIN_MESSAGE, null, null, null);
 			System.out.println(user);
-			
-			String pass = (String)JOptionPane.showInputDialog(this.adminView,
-					"Password?",
-					"Admin Register",
-					JOptionPane.PLAIN_MESSAGE,
-					null,
-					null,
-					null);
+
+			String pass = (String) JOptionPane.showInputDialog(this.adminView, "Password?", "Admin Register",
+					JOptionPane.PLAIN_MESSAGE, null, null, null);
 			System.out.println(pass);
-			
+
 			AdminDBQ data = new AdminDBQ(this.adminView);
 			data.createAdmin(user, pass);
 			adminView.UpdateFrame();
-			
-			}
-		
-		if(e.getActionCommand().equals("Delete Admin")) {
+
+		}
+
+		// TO DELETE ADMINISTRATOR SELECTED ON TABLE BY ADMINISTRATOR ID
+		if (e.getActionCommand().equals("Delete Admin")) {
 			String errorMg = "Ups, there is an internal problem!! Check the code";
 			String confMg = "The admin has been DELETED!";
 			AdminDBQ data = new AdminDBQ(this.adminView);
-			data.deleteRow("administrators", "admin_id", this.adminView.getDataAdmin(this.adminView.getSelectedRowAdmin(), 0), errorMg, confMg);
+			data.deleteRow("administrators", "admin_id",
+					this.adminView.getDataAdmin(this.adminView.getSelectedRowAdmin(), 0), errorMg, confMg);
 			adminView.UpdateFrame();
 		}
-		
-		if(e.getActionCommand().equals("Update Tables")) {
+
+		// TO UPDATE ALL TABLES IN ADMINISTRATOR MANAGER
+		if (e.getActionCommand().equals("Update Tables")) {
 			adminView.UpdateFrame();
-			JOptionPane.showMessageDialog(this.adminView, "Tables have been UPDATED!", "Update", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(this.adminView, "Tables have been UPDATED!", "Update",
+					JOptionPane.INFORMATION_MESSAGE);
 		}
-		
-		if(e.getActionCommand().equals("Logout")) {
+
+		// TO LOGOUT
+		if (e.getActionCommand().equals("Logout")) {
 			JOptionPane.showMessageDialog(this.adminView, "See you soon!", "Logout", JOptionPane.INFORMATION_MESSAGE);
 			this.adminView.getAdminView().dispose();
 			this.login.getLogin().setVisible(true);
 		}
 
-}
+	}
 }
--- src/ProviderController.java
+++ src/ProviderController.java
@@ -8,76 +8,79 @@ public class ProviderController implements ActionListener {
 	// GLOBAL VARIABLES - DECLARATION
 	private ProviderView proView;
 	private Login login;
-	
+
 	// CONSTRUCTOR
 	public ProviderController(String email, Login Login) {
-		
+
 		// NEW INSTANCE OF PROVIDERVIEW THAT ALSO INSTANCE AN VIEW OBJECT FOR PROVIDER VIEW
 		this.proView = new ProviderView(this, email);
 		this.login = Login;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		// TO ADD AN AVAILABILITY
-		if(e.getActionCommand().equals("Add")) {
-			
+		if (e.getActionCommand().equals("Add")) {
+
 			ProviderDBQ proDB = new ProviderDBQ(this.proView);
 			proDB.addAvailability();
-			
+
 		}
-		
+
 		// TO CONFIRM APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if (e.getActionCommand().equals("Confirm")) {
 			ProviderDBQ proDB = new ProviderDBQ(this.proView);
-			String avai_ref = this.proView.getDataBooked(this.proView.getSelectedRowT(),0);
+			String avai_ref = this.proView.getDataBooked(this.proView.getSelectedRowT(), 0);
 			proDB.comfirmAppointPro(avai_ref);
 		}
-		
+
 		// TO DELETE AVAILABILITY SELECTED ON TABLE BY AVAILABILITY REFERENCE
-		if(e.getActionCommand().equals("Delete")) {
+		if (e.getActionCommand().equals("Delete")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
-	      	String confMg = "The availability has been DELETED!";
+			String confMg = "The availability has been DELETED!";
 			ProviderDBQ proDB = new ProviderDBQ(this.proView);
-			proDB.deleteRow("availabilities", "avai_ref", this.proView.getDataAvai(this.proView.getSelectedRowTwo(), 0), errorMg, confMg);
+			proDB.deleteRow("availabilities", "avai_ref", this.proView.getDataAvai(this.proView.getSelectedRowTwo(), 0),
+					errorMg, confMg);
 			proView.UpdateFrame();
 		}
-		
+
 		// TO CANCEL APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if (e.getActionCommand().equals("Cancel")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "Appointment has been CANCELLED!";
 			ProviderDBQ proDB = new ProviderDBQ(this.proView);
-			proDB.cancelAppointPro(this.proView.getDataBooked(this.proView.getSelectedRowT(), 0), errorMg, confMg);;
+			proDB.cancelAppointPro(this.proView.getDataBooked(this.proView.getSelectedRowT(), 0), errorMg, confMg);
+			;
 			this.proView.UpdateFrame();
 		}
-		
+
 		// TO SET AS COMPLETED TO AN APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
 		if (e.getActionCommand().equals("Completed")) {
 			String errorMg = "Ups, there is an internal problem, please contact an administrator";
 			String confMg = "Appointment has been set COMPLETED!";
 			ProviderDBQ proDB = new ProviderDBQ(this.proView);
-			proDB.setCompletedAppointPro(this.proView.getDataBooked(this.proView.getSelectedRowT(), 0), errorMg, confMg);;
+			proDB.setCompletedAppointPro(this.proView.getDataBooked(this.proView.getSelectedRowT(), 0), errorMg,
+					confMg);
+			;
 			this.proView.UpdateFrame();
 		}
-		
+
 		// TO SHOW COMMENTS OF AN APPOINTMENT SELECTED ON TABLE BY APPOINTMENT REFERENCE
-		if(e.getActionCommand().equals("Show")) {
-			
+		if (e.getActionCommand().equals("Show")) {
+
 			JOptionPane.showMessageDialog(this.proView, this.proView.getDataBooked(this.proView.getSelectedRowT(), 6));
-			
+
 		}
-		
+
 		// TO LOGOUT
-		if(e.getActionCommand().equals("Logout")) {
+		if (e.getActionCommand().equals("Logout")) {
 			JOptionPane.showMessageDialog(this.proView, "See you soon!", "Logout", JOptionPane.INFORMATION_MESSAGE);
 			this.proView.getProView().dispose();
 			this.login.getLogin().setVisible(true);
 		}
-		
-		
+
 	}
 
 }

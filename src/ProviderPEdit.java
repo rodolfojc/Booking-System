8260795425df2ff52f5f267be9030369e67c43f8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-01-20 21:10:05 +0000

    ProviderPEdit - Variables and Constructor
    Global variables and default constructor have been set.

--- src/ProviderPEdit.java
+++ src/ProviderPEdit.java
@@ -1,12 +1,34 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class ProviderPEdit {
+public class ProviderPEdit extends JFrame {
 
 	private View proPedit;
 	private ProviderView proView;
-	private ProviderController providerController;
+	private ProviderController proController;
+	private String currentEmail, currentMob, currentAddress, currentLocation;
 	
 		
-	public ProviderPEdit() {
+	public ProviderPEdit(ProviderController ProController, ProviderView ProView ) {
+		
+		this.proView = ProView;
+		this.proController = ProController;
+		this.currentEmail = this.proView.getProviderEmail();
+		this.currentMob = this.proView.getProviderMobile();
+		this.currentAddress = this.proView.getProviderAddress();
+		this.currentLocation = this.proView.getProviderLocation();
+		this.proPedit = new View("Provider Profile Editor", 500, 300, true);
+		this.proPedit.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setupFrame();
+	}
+
+	public void setupFrame() {
 		
 	}
+	
+	
+	
+	
 }

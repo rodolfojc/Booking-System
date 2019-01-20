ca4906ad2cda1a6bc320a988d9e9fd2c7d0683d0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-01-20 21:20:01 +0000

    ProviderPEdit - Address
    Address field has been set.

--- src/ProviderPEdit.java
+++ src/ProviderPEdit.java
@@ -9,7 +9,9 @@ public class ProviderPEdit extends JFrame {
 	private ProviderView proView;
 	private ProviderController proController;
 	private String currentEmail, currentMob, currentAddress, currentLocation;
-	
+	private JTextField email, mob, address, location;
+	private JButton editEmail, setNewEmail, editMob, setNewMob, editAddress, setNewAddress, editLocation, setNewLocation, submit, cancel;
+	private boolean emailFlag = true, mobileFlag = true, addressFlag = true, locationFlag, submitFlag = true;
 		
 	public ProviderPEdit(ProviderController ProController, ProviderView ProView ) {
 		
@@ -25,6 +27,26 @@ public class ProviderPEdit extends JFrame {
 	}
 
 	public void setupFrame() {
+		
+		this.proPedit.setBox(this.proPedit.panel, 1);
+		
+		JPanel newEmail = new JPanel();
+		JPanel newMob = new JPanel();
+		JPanel newAddress = new JPanel();
+		JPanel newLocation = new JPanel();
+		JPanel buttons = new JPanel();
+		
+		this.proPedit.addLabel("Email ", newEmail);
+		this.email = this.proPedit.addTextField(20, newEmail);
+		this.email.setText(this.currentEmail);
+		if (this.emailFlag) {this.email.setEditable(false);}		
+		this.editEmail = this.proPedit.addButton("Edit",newEmail);
+		this.editEmail.setActionCommand("Edit Email");
+		this.editEmail.addActionListener(proController);
+		this.setNewEmail = this.proPedit.addButton("Set", newEmail);
+		this.setNewEmail.setActionCommand("Set Email");
+		this.setNewEmail.addActionListener(proController);
+		
 		
 	}
 	
